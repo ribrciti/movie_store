1a974b3f5870faee333c186cca405e58e4bf3b2a
ribrciti <rbciti@msn.com>
2015-10-24 09:15:33 -0700

    Added css styles with foundation and devise authentication

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
+  resources :movies, only: [:show, :index]
+
+  root 'movies#index'
+
+end
+
+
+
+
+ 
